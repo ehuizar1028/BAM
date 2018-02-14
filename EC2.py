ba07ec14ce83b182ec68813ebb3826dcffe4a878
ehuizar1028 <33471880+ehuizar1028@users.noreply.github.com>
2018-02-14 09:24:02 -0600

    Add files via upload

--- EC2.py
+++ EC2.py
@@ -8,33 +8,32 @@ class EC2():
 
     def createInstance(self):
         if len(sys.argv) < 1:
-            print "There are no arguments specified, using the default parameters"
-        self.args = self.argParse()
-        self.EC2Config(self.args)
+            print "There are no arguments specified, using the default parameters" ##If there are no arguments or optinons used, then prints out that default will be used
+        self.args = self.argParse() ##Parses the args
+        self.EC2Config(self.args) ##passes the parsed args to the configuration function
 
     def argParse(self):
         p = argparse.ArgumentParser()
-        p.add_argument('--VolumeSize', default=8,required=False)
-        p.add_argument('--VolumeType', default='standard',required=False)
-        p.add_argument('--InstanceType', default='t2.micro', required=False)
-        p.add_argument('--ImageId', default='ami-97785bed', required=False)
-        p.add_argument('--AZ', default='us-east-1a', required=False)
-        p.add_argument('--SubnetId', default='', required=False)
-        p.add_argument('--SecurityGroupIds', default='', required=False)
-        a = p.parse_args()
+        p.add_argument('--VolumeSize', default=8,required=False) ##defines volumeSize
+        p.add_argument('--VolumeType', default='standard',required=False) ##defines volumeType
+        p.add_argument('--InstanceType', default='t2.micro', required=False) ##defines InstanceType
+        p.add_argument('--ImageId', default='ami-97785bed', required=False) ##Defines ImageID
+        p.add_argument('--AZ', default='us-east-1a', required=False) ##Defines AZ Zone
+        p.add_argument('--SubnetId', default='', required=False) ##Defines Subnet
+        p.add_argument('--SecurityGroupIds', default='', required=False) ##Defines list of Security Groups
+        a = p.parse_args() ##Parses the arguments as a namespace
         print 'Setting up instance with the following arguments: '
-        for key,value in vars(a).iteritems():
-            print "{} : {}".format(key, value)
-        return a
+        for key,value in vars(a).iteritems(): ##Converts namespace to Dict
+            print "{} : {}".format(key, value) #Prints Dict key value pairs
+        return a ##Returns the parsed arguments
 
     def EC2Config(self,args):
-        session = boto3.Session(aws_access_key_id='YOUR_KEY', aws_secret_access_key='YOUR_SECRET_KEY')
-
+        region = args.AZ ##defines the region
+        session = boto3.Session(aws_access_key_id='AKIAI7O6CWOSFOJFQ4VQ', aws_secret_access_key='neYiVS8LEJDPxcc8aMNSoJXi9nCWhG4PIXAHywdH', region_name=region[:-1]) ##input the connection credentials
         if args.SecurityGroupIds is None or args.SecurityGroupIds == '':
-            secGroups = []
+            secGroups = [] ##If there is no security group defined, use empty list, which is equal to default parameter
         else:
-            secGroups = args.SecurityGroupIds.split(',')
-        print args.SecurityGroupIds
+            secGroups = args.SecurityGroupIds.split(',') ##Split the list given for Security Groups, convert to list
         bootstrap = """
                     #!/bin/bash 
                     yum -y update
@@ -42,8 +41,9 @@ class EC2():
                     chkconfig httpd on
                     service httpd start;
                     echo "AWS Automation is Fun!" > /var/www/html/index.html
-                    """
-        inst = session.resource('ec2')
+                    """ ##Boot Strap Script for installing necessary items (apache, updates, etc)
+        inst = session.resource('ec2') ##createing the session and connecting to resource
+        #inst = boto3.resource('ec2') ##To use config at ~/.aws/credentials, uncomment this, will overwrite the object
         inst.create_instances(
             ImageId=args.ImageId,
             InstanceType=args.InstanceType,
@@ -54,6 +54,6 @@ class EC2():
             SecurityGroupIds=secGroups,
             MinCount=1,
             MaxCount=1
-        )
+        ) ##Start the provisionsing
 
-c = EC2().createInstance()
+c = EC2().createInstance() ##Creating objects and calling the functions
